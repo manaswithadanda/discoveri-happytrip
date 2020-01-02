55a2331d0a864bb4ec016e8a925c302940db068e
manaswitha <manaswitha.danda@pratian.com>
2020-01-02 13:57:04 +0530

    Test Cases

---
+++ src/test/java/testHappyTrip/HTTestLogin.java
@@ -0,0 +1,33 @@
+package testHappyTrip;
+
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HTTestLogin {
+	
+	WebDriver driver;
+	String chromeDriverPath = "./src/test/resources/drivers/chromedriver.exe";
+	String URL = "http://172.30.15.99:8085/HappyTrip";
+	WebDriverWait wait;
+	
+	@Test
+	public void testLogin() {
+		
+		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(URL);
+
+		WebElement ele = driver.findElement(By.id("button_flight_search"));
+		ele.click();
+		
+		assertTrue(driver.findElement(By.xpath("//*[@id='errorContainer']/li[2]")).getText().contains("Location is empty"));
+		
+	}
+}
